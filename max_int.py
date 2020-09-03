0ce72e9fe441c0a6c5bdc2b345f3d027db526fea
Grímur Valur Gunnarsson <grimur@Grimurs-MacBook-Pro.local>
2020-09-03 10:30:08 +0000

    max_int.py

---
+++ max_int.py
@@ -0,0 +1,7 @@
+#assign max_int to zero
+
+#make a while statement that stops when num_int goes below zero
+
+#make a if statement that states if num_int is bigger than max_int then max_int equals num_int
+
+#print out max_int
